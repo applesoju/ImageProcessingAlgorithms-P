0fbd93206f97e22aff736cf0957456f296a88489
AppleSoju <patryknmarciniak@gmail.com>
2022-11-22 21:25:47 +0100

    added methods for showing images and histograms from each category and for computing and displaying mean histograms for each category

--- image_dataset.py
+++ image_dataset.py
@@ -2,6 +2,7 @@ import random
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 import process_img as pimg
 from mri_image import MriImage
@@ -13,6 +14,7 @@ class ImageDataset:
     def __init__(self) -> None:
         self.image_list = []
         self.categories = []
+        self.mean_histograms = []
 
     # Load images from given path to the dataset
     def load_images(self, dir_path) -> None:
@@ -34,7 +36,25 @@ class ImageDataset:
         random_img.show_image()
 
     # Show an image from every category
-    # def show_image_from_all_cat(self, scale) -> None:
+    def show_image_from_each_cat(self) -> None:
+        cat_count = len(self.categories)
+        layout = (cat_count // 2, 2) if cat_count % 2 == 0 else (cat_count, 1)
+
+        images_by_categories = [
+            [img for img in self.image_list if img.category == category] for category in self.categories
+        ]
+
+        random_indexes = [random.randint(0, len(cat)) for cat in images_by_categories]
+        for i, idx in enumerate(random_indexes):
+            img = images_by_categories[i][idx]
+
+            plt.subplot(layout[0], layout[1], i + 1)
+            plt.imshow(img.image)
+            plt.xlabel('Pixel value')
+            plt.ylabel('Pixel count')
+            plt.title(f'Name = {img.name}, Category = {img.category}')
+
+        plt.show()
 
     # Show a random image from the specified category
     def show_image_from_category(self, category) -> None:
@@ -54,33 +74,75 @@ class ImageDataset:
         random_img = self.image_list[random_index]
         random_img.show_histogram(scale)
 
-    # Show a histogram of an image from every category
-    # def show_histogram_from_all_cat(self, scale) -> None:
-    #     img_from_cat = []
-    #     cat_count = len(self.categories)
-    #
-    #     for cat in self.categories:
-    #         img_from_cat = [img for img in self.image_list if img.category == cat]
-    #
-    #     random_indexes = [random.randint(0, len(img_from_cat[i])) for i in self.categories]
-    #     images = [img_from_cat[i][idx] for idx, i in (random_indexes, self.categories)]
-    #
-    #     layout = (cat_count // 2, 2) if cat_count % 2 == 0 else (cat_count, 1)
-    #     plt.subplots(layout[0], layout[1], figsize=(16, 9))
-    #
-    #     for i, img in enumerate(images):
-    #         hist = cv2.calcHist([img.image],
-    #                             [0],
-    #                             None,
-    #                             [256],
-    #                             [0, 256])
-    #         plt.subplot(layout[0] * 100 +
-    #                     layout[1] * 10 + i + 1)
-    #         plt.plot(hist, color='black')
-    #         plt.xlabel('Pixel value')
-    #         plt.ylabel('Pixel count')
-    #         plt.title(f'Name = {img.name}, Category = {img.category}')
-    #         plt.grid()
-    #         plt.yscale(scale)
-    #
-    #     plt.show()
+    def show_hist_from_each_cat(self, scale) -> None:
+        cat_count = len(self.categories)
+        layout = (cat_count // 2, 2) if cat_count % 2 == 0 else (cat_count, 1)
+
+        images_by_categories = [
+            [img for img in self.image_list if img.category == category] for category in self.categories
+        ]
+
+        random_indexes = [random.randint(0, len(cat)) for cat in images_by_categories]
+        for i, idx in enumerate(random_indexes):
+            img = images_by_categories[i][idx]
+            hist = cv2.calcHist([img.image],
+                                [0],
+                                None,
+                                [256],
+                                [0, 256])
+
+            plt.subplot(layout[0], layout[1], i + 1)
+            plt.plot(hist, color='black')
+            plt.xlabel('Pixel value')
+            plt.ylabel('Pixel count')
+            plt.title(f'Name = {img.name}, Category = {img.category}')
+            plt.grid()
+            plt.yscale(scale)
+
+        plt.show()
+
+    def compute_mean_hist(self) -> None:
+        mean_histograms = []
+
+        images_by_categories = [
+            [img for img in self.image_list if img.category == category] for category in self.categories
+        ]
+
+        for idx, _ in enumerate(self.categories):
+            sample_img = images_by_categories[idx][0]
+            if not sample_img.histogram:
+                sample_img.create_histogram()
+
+            img_shape = images_by_categories[idx][0].histogram.shape
+            mean_image = np.zeros(shape=img_shape)
+
+            for img in images_by_categories[idx]:
+                if img.histogram is None:
+                    img.create_histogram()
+
+                mean_image = np.add(img.histogram, mean_image)
+
+            mean_image = mean_image / len(images_by_categories[idx])
+            mean_histograms.append(mean_image)
+
+        self.mean_histograms = mean_histograms
+
+    def show_mean_histograms(self, scale):
+        cat_count = len(self.categories)
+        layout = (cat_count // 2, 2) if cat_count % 2 == 0 else (cat_count, 1)
+
+        if not self.mean_histograms:
+            self.compute_mean_hist()
+
+        for i, mean_hist in enumerate(self.mean_histograms):
+
+            plt.subplot(layout[0], layout[1], i + 1)
+            plt.plot(mean_hist, color='black')
+            plt.xlabel('Pixel value')
+            plt.ylabel('Pixel count')
+            plt.title(f'Mean Histogram, Category = {self.categories[i]}')
+            plt.grid()
+            plt.yscale(scale)
+
+        plt.show()
+
--- main.py
+++ main.py
@@ -8,4 +8,6 @@ if __name__ == '__main__':
     # mri_images_dataset.show_random_image()
     # mri_images_dataset.show_image_from_category('ModerateDemented')
     # mri_images_dataset.show_random_histogram(scale='log')
-    # mri_images_dataset.show_image_from_all_cat(scale='log')
+    # mri_images_dataset.show_image_from_each_cat()
+    # mri_images_dataset.show_hist_from_each_cat(scale='log')
+    # mri_images_dataset.show_mean_histograms(scale='log')
