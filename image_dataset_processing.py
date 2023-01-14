369262be2ba7dd692ac4765e956ff9e049b9db0a
AppleSoju <patryknmarciniak@gmail.com>
2023-01-14 21:38:02 +0100

    removed unecessary functions and rewrote the class so it focuses on generating features for a dataset

--- image_dataset_processing.py
+++ image_dataset_processing.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from os import path
 
 import numpy as np
@@ -7,87 +8,136 @@ import pandas as pd
 import process_img as pimg
 from mri_image import MriImage
 
-FEATURES_PARAMS = {
-    'lbp': [2, 4, 'uniform'],
-    'zm': [2],
-    'glcm': [[2], [0, np.pi / 4, np.pi / 2, np.pi * 3 / 4]]
-}
+LBP_PARAMS = [[1, 2, 3], 8, 'uniform']
+ZM_PARAMS = [[1, 2, 3, 4]]
+GLCM_PARAMS = [[1, 2, 3], [0, np.pi / 12, np.pi / 6, np.pi / 4,     # DEG: 0, 15, 30, 45
+                           np.pi / 3, np.pi * 5 / 12, np.pi / 2, np.pi * 7 / 12,    # DEG 60, 75, 90, 105
+                           np.pi * 2 / 3, np.pi * 3 / 4, np.pi * 5 / 6, np.pi * 11 / 12]]   # DEG 120, 135, 150, 165
+
+class ImageDatasetProcessing:
+    # Processing process of an image dataset
+
+    def __init__(self, dir_path) -> None:
+        self.images = {}
+        self.dataset_features = None
+
+        class_dirs = os.listdir(dir_path)
+        for cdir in class_dirs:
+            self.images[cdir] = []
 
 
-class ImageDataset:
-    # Represents a dataset that contains some images
+            class_path = f'{dir_path}/{cdir}'
+            file_list = os.listdir(class_path)
 
-    def __init__(self) -> None:
-        self.image_list = []
-        self.categories = []
+            for f in file_list:
+                file_path = f'{class_path}/{f}'
+                new_image = MriImage(name=f, category=cdir, file_path=file_path)
 
-    # Load images from given path to the dataset
-    def load_images(self, dir_path) -> None:
-        file_path_list = pimg.get_images_path_list(dir_path)
-
-        for file_path in file_path_list:
-            _, category, name = file_path.split('/')
-
-            if category not in self.categories:
-                self.categories.append(category)
-
-            new_image = MriImage(name, category, file_path)
-            self.image_list.append(new_image)
-
-    # Create histograms of all images
-    def generate_histograms(self) -> None:
-        for img in self.image_list:
-            img.create_histogram()
-
-    # Generate Fourier transforms for all images
-    def generate_ffts(self) -> None:
-        for img in self.image_list:
-            img.create_fft()
+                self.images[cdir].append(new_image)
 
     # Generate LBP of all images using given parameters
-    def generate_lbps(self, radius, n_points, method) -> None:
-        for img in self.image_list:
-            img.create_lbp(radius, n_points, method)
+    def generate_lbps(self, radius, n_points, method, verbose=False) -> pd.DataFrame:
+        if verbose:
+            print('Generating LBPs...')
+
+        lbps_list = []
+
+        for c in self.images:
+            for img in self.images[c]:
+
+                lbp_out = img.generate_lbps(radius, n_points, method)
+                lbps_list.append(lbp_out)
+
+            if verbose:
+                print(f'LBPs of class {c} finished.')
+
+        lbps_df = pd.concat(lbps_list)
+        lbps_df = lbps_df.reset_index(drop=True)
+
+        if verbose:
+            print('Finished generating LBPs.')
+
+        return lbps_df
 
     # Generate Zernike Moments of all images
-    def generate_zernike_moments(self, radius) -> None:
-        for img in self.image_list:
-            img.create_zernike_moments(radius)
+    def generate_zernike_moments(self, radius, verbose=False) -> pd.DataFrame:
+        if verbose:
+            print('Generating Zernike Moments...')
+
+        zm_list = []
+
+        for c in self.images:
+            for img in self.images[c]:
+
+                zm_out = img.generate_zernike_moments(radius)
+                zm_list.append(zm_out)
+
+            if verbose:
+                print(f'Zernike Moments of class {c} finished.')
+
+        zm_df = pd.concat(zm_list)
+        zm_df = zm_df.reset_index(drop=True)
+
+        if verbose:
+            print('Finished generating Zernike Moments.')
+
+        return zm_df
+
 
     # Generate Gray Level Co-occurance Matrices for all images
-    def generate_glcm(self, distances, angles, levels=256, symmetric=True, normed=True) -> None:
-        for img in self.image_list:
-            img.calculate_glcm(distances, angles, levels, symmetric, normed)
+    def generate_glcm(self, distances, angles, levels=256,
+                      symmetric=True, normed=True, verbose=False) -> pd.DataFrame:
+        if verbose:
+            print('Generating GLCMs...')
 
-    # Call all methods with default values
-    def generate_features(self) -> None:
-        sample_image = self.image_list[-1]
+        glcm_list = []
 
-        if sample_image.lbp is None:
-            self.generate_lbps(FEATURES_PARAMS['lbp'][0],
-                               FEATURES_PARAMS['lbp'][1],
-                               FEATURES_PARAMS['lbp'][2])
+        for c in self.images:
+            for img in self.images[c]:
 
-        if sample_image.glcm is None:
-            self.generate_glcm(FEATURES_PARAMS['glcm'][0],
-                               FEATURES_PARAMS['glcm'][1])
+                glcm_out = img.generate_glcm(distances, angles, levels, symmetric, normed)
+                glcm_list.append(glcm_out)
 
-        if sample_image.zernike is None:
-            self.generate_zernike_moments(FEATURES_PARAMS['zm'][0])
+            if verbose:
+                print(f'GLCM of class {c} finished.')
 
-    def save_dataset(self, path_to_dir):
-        self.generate_features()
+        glcm_df = pd.concat(glcm_list)
+        glcm_df = glcm_df.reset_index(drop=True)
 
-        cats = self.categories
+        if verbose:
+            print('Finished generating GLCMs.')
+
+        return glcm_df
+
+    def generate_features_for_dataset(self, lbp_params, zernike_params, glcm_params, verbose=False):
+
+        lbp = self.generate_lbps(lbp_params[0],
+                                 lbp_params[1],
+                                 lbp_params[2],
+                                 verbose=verbose)
+        glcm = self.generate_glcm(glcm_params[0],
+                                  glcm_params[1],
+                                  verbose=verbose)
+        zernike = self.generate_zernike_moments(zernike_params[0],
+                                                verbose=verbose)
+
+        self.dataset_features = lbp.join(glcm).join(zernike)
+
+        return self.dataset_features
+
+    def save_features_to_csv(self, path_to_dir, file_name, verbose=False):
+        feature_df = self.dataset_features
+
+        if feature_df is None:
+           feature_df = self.generate_features_for_dataset(LBP_PARAMS,
+                                                           ZM_PARAMS,
+                                                           GLCM_PARAMS,
+                                                           verbose=verbose)
 
         if not path.exists(path_to_dir):
-            os.makedirs(path_to_dir)
+            subprocess.call(['mkdir', path_to_dir], shell=False)
 
-        for cat in cats:
-            cat_path = f'{path_to_dir}/{cat}'
+        if file_name[-4:] != '.csv':
+            file_name += '.csv'
 
-            features = [img.get_all_features() for img in self.image_list if img.category == cat]
-            cat_df = pd.concat(features)
-
-            file_name = f'{cat_path}.csv'
-            cat_df.to_csv(file_name)
+        feature_df.to_csv(f'{path_to_dir}/{file_name}')
