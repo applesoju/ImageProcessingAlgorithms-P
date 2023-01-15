25a0e3f8e183aca054cf75fc3d55c21e69b163fd
AppleSoju <patryknmarciniak@gmail.com>
2023-01-15 14:24:54 +0100

    adjusted main.py for new functionality

--- main.py
+++ main.py
@@ -7,29 +7,37 @@ PROCESSED_DIR_NAME = 'feature_processing'
 FEATURES_FILE_NAME = 'features_df'
 
 if __name__ == '__main__':
-    # Create class and load images by class from a given dir
-    dataset_proc = ImageDatasetProcessing(RESOURCE_DIR_NAME, verbose=True)
-    print('---------------------------------------------------------------')
+    # # Create class and load images by class from a given dir
+    # dataset_proc = ImageDatasetProcessing(RESOURCE_DIR_NAME, verbose=True)
+    # print('---------------------------------------------------------------')
+    #
+    # # Generate LBP, Zernike Moments and Gray Level Co-occurance Matrix with given parameters for every image
+    # dataset_proc.generate_features_for_dataset(LBP_PARAMS,
+    #                                            ZM_PARAMS,
+    #                                            GLCM_PARAMS)
+    # print('---------------------------------------------------------------')
+    #
+    # # Save a DataFrame containing all features to a csv file
+    # filepath = dataset_proc.save_features_to_csv(PROCESSED_DIR_NAME,
+    #                                              FEATURES_FILE_NAME)
+    # print('---------------------------------------------------------------')
 
-    # Generate LBP, Zernike Moments and Gray Level Co-occurance Matrix with given parameters for every image
-    dataset_proc.generate_features_for_dataset(LBP_PARAMS,
-                                               ZM_PARAMS,
-                                               GLCM_PARAMS)
-    print('---------------------------------------------------------------')
-
-    # Save a DataFrame containing all features to a csv file
-    filepath = dataset_proc.save_features_to_csv(PROCESSED_DIR_NAME,
-                                                 FEATURES_FILE_NAME)
-    print('---------------------------------------------------------------')
-
-    # filepath = 'feature_processing/features_df.csv'
+    # Use this variable if features were already determined
+    filepath = 'feature_processing/features_df.csv'
 
     # Create class and load a DataFrame containing features
     feature_proc = FeatureProcessing(filepath, verbose=True)
 
     # Determine a given number of best features
-    best_features = feature_proc.get_best_features(20)
+    best_features = feature_proc.get_best_features(30)
     print('---------------------------------------------------------------')
+
+    # Perform Principal Component Analysis
+    pca = feature_proc.perform_pca(best_features, n_components=3)
+    print('---------------------------------------------------------------')
+
+    # Perform
+
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # norm_arr_mri = dfp.normalize_df(final_df, best_features)
