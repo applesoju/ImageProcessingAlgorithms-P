a7445f10fe3fff0bfbdee40923a369cd984ecf36
AppleSoju <patryknmarciniak@gmail.com>
2023-01-15 01:26:01 +0100

    created FeatureProcessing class and removed unecessary functions

--- feature_processing.py
+++ feature_processing.py
@@ -17,69 +17,20 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 import image_dataset_processing
 
 
-def save_distributions_of_feature(class_dict, feature, fig_path):
-    class_feature_df = pd.DataFrame()
+class FeatureProcessing:
 
-    for cd in class_dict:
-        mdf = class_dict[cd]
+    def __init__(self, df_or_csv):
+        if type(df_or_csv) is pd.DataFrame:
+            self.features_df = df_or_csv
 
-        class_feature_df[cd] = mdf.df[feature]
+        elif type(df_or_csv) is str:
+            if not os.path.exists(df_or_csv):
+                raise FileNotFoundError
 
-    sn.displot(class_feature_df, kind='kde', common_norm=False)
-    plt.savefig(fig_path)
-    plt.close()
+            self.features_df = pd.read_csv(df_or_csv)
 
-
-def save_dist_of_all_features(class_dict, dir_path):
-    if not os.path.exists(dir_path):
-        subprocess.call(['mkdir', dir_path], shell=True)
-
-    features = next(iter(class_dict.values())).features
-
-    for f in features[1:]:
-        fp = image_dataset.FEATURES_PARAMS
-
-        dir_for_figs = f"{fp['lbp'][0]}-{fp['lbp'][1]}-{fp['lbp'][2]}_" \
-                       f"{fp['zm'][0]}_" \
-                       f"{int(fp['glcm'][0][0])}-" \
-                       f"{int(math.degrees(fp['glcm'][1][0]))}-{int(math.degrees(fp['glcm'][1][1]))}-" \
-                       f"{int(math.degrees(fp['glcm'][1][2]))}-{int(math.degrees(fp['glcm'][1][3]))}"
-
-        fig_dir_path = f'{dir_path}\\{dir_for_figs}'
-        if not os.path.exists(fig_dir_path):
-            subprocess.call(['mkdir', fig_dir_path], shell=True)
-
-        figure_path = f'{fig_dir_path}/{f}.png'
-        save_distributions_of_feature(class_dict, f, figure_path)
-
-
-def get_xy_arrays_from_dfs(dfs):
-    feature_list = []
-    class_list = []
-
-    for key in dfs:
-        features, classes = dfs[key].get_as_list(list(dfs.keys()))
-
-        feature_list += features
-        class_list += classes
-
-    return np.array(feature_list), np.array(class_list)
-
-
-def get_one_dataframe(mri_dfs_to_join):
-    dfs_to_join = [mri_dfs_to_join[cat].df for cat in mri_dfs_to_join]
-
-    for df, cat in zip(dfs_to_join, mri_dfs_to_join):
-        df['Class'] = cat
-
-    final_df = pd.concat(dfs_to_join)
-    final_df = final_df.drop(['file_name'], axis=1)
-
-    label_enc = LabelEncoder()
-    final_df['Class'] = label_enc.fit_transform(final_df['Class'])
-
-    return final_df, label_enc.classes_
-
+        else:
+            raise TypeError
 
 def get_best_features(x, y, col, n_features='all'):
     skb = SelectKBest(mutual_info_classif, k=n_features)
